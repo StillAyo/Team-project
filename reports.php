b80f7a06505fe14e8c82b653c9de9dab309c5f3e
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-11 23:28:33 +0000

    Update reports.php

--- reports.php
+++ reports.php
@@ -17,7 +17,7 @@
   <!-- CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href="css/dashboard.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="home.css">
+  <link rel="newest stylesheet" type="text/css" href="home.css">
   <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
   
 
