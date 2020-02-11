639360f277ad318b990d275549d7748638acedf0
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-11 21:02:45 +0000

    Update reports.php

--- reports.php
+++ reports.php
@@ -47,15 +47,7 @@
 					echo "Connected now <br/>";
 			}
 			
-			$sql="SELECT type, COUNT(type) FROM live JOIN problem USING (problem_no) GROUP BY type";
-			$result = mysqli_query($conn, $sql);
-			// Fetch all
-			$test = mysqli_fetch_all($result, MYSQLI_NUM);
-
-			// Free result set
 			
-			echo $test[0][0];
-			print_r($test);
 			?>
 			
 			/*
