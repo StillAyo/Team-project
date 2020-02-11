bf89b2d0a3b6cf12507ae43625ebc56b565f9aa4
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-11 22:29:11 +0000

    Update reports.php

--- reports.php
+++ reports.php
@@ -47,8 +47,11 @@
 				$test = json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
 				
 			?>
-			var temp = JSON.parse("<?php echo $test?>");
+			var temp = JSON.parse('<?php echo $test?>');
 			console.log(temp);
+			var pie_chart_data = [];
+			var headings = ['Problem type', 'Amount'];
+			
 	});	
     </script>
 </head>
