87654707c79e0df3f4293dbbf4c3c3adf36b6890
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-12 00:37:20 +0000

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -77,7 +77,7 @@ button:hover {
 	$res = mysqli_query($conn, $sql);
 		// Fetch all
 	$result = mysqli_fetch_all($res, MYSQLI_ASSOC);
-	echo $result;
+	print_r($result);
 
 	// $types = array('integer','integer', 'text', 'text'); //Data types of values
 	// $sth = $mdb2->prepare('INSERT INTO call_log (callerid, operatorid, time, reason)VALUES (?, ?, ?, ?)', $types, MDB2_PREPARE_MANIP); //Sql prepare statement
