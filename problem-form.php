9b326290d4711ed0a846b2c53443cba7c4fdf2a2
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-12 08:36:58 +0000

    Update problem-form.php

--- problem-form.php
+++ problem-form.php
@@ -107,12 +107,12 @@ button:hover {
 					$conn = mysqli_connect($servername, $username, $password, 'team018');
 
 					// Check connection
-					 if ($conn->connect_error) {
+					if ($conn->connect_error) {
 						die("Connection failed: " . $conn->connect_error);
 					}
 					else{
 							echo "Connected successfully";
-					} 
+					}
                     
                     
                    
