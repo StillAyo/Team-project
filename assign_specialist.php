9b4e4134656d9e037a293b24095d807e6a714e45
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-13 02:46:23 +0000

    Update assign_specialist.php

--- assign_specialist.php
+++ assign_specialist.php
@@ -81,18 +81,9 @@ body {
 	$problem_no = $_SESSION["Problem_no"];
 	
 	$fields_values = array($problem_no, '123');
-	
+	print_r($fields_values);
 	//----------------CHANGE '123' TO SPECIALIST ID-------------------------//
-	$sql = 'INSERT INTO live (problem_no, specialist_id) VALUES ($fields_values[0], $fields_values[1])'; //Sql prepare statement
-	if (mysqli_query($conn, $sql)) {
-		echo "New record created successfully";
-	} else {
-		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-	}
-	else{
-			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-
-		}
+	
 	
 	
 
