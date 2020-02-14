437f122d0ee14ca977e36a9250a183d8bb2da695
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-14 17:08:13 +0000

    Update liveJobs.php

--- liveJobs.php
+++ liveJobs.php
@@ -7,11 +7,11 @@ $password = "SIpnz0Sjel";
 $conn = mysqli_connect($servername, $username, $password, 'team018');
 
 
-$sql = "SELECT * from live";
+$sql = "SELECT live.problem_no, type, personnel_id as CallerID, date FROM live INNER JOIN problem ON live.problem_no = problem.problem_no";
 
 $res = mysqli_query($conn, $sql) or trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error($conn), E_USER_ERROR);
 
 // Fetch all
-$result = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$result = mysqli_fetch_all($res, MYSQLI_ASSOC);
  
-echo ($result);
+echo json_encode($result);
