20446c56ac7a12887b5fb3ce59154f314ced7c1c
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-14 17:12:10 +0000

    Update liveJobs.php

--- liveJobs.php
+++ liveJobs.php
@@ -12,6 +12,15 @@ $sql = "SELECT live.problem_no, type, personnel_id as CallerID, date FROM live I
 $res = mysqli_query($conn, $sql) or trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error($conn), E_USER_ERROR);
 
 // Fetch all
-$result = mysqli_fetch_all($res, MYSQLI_ASSOC);
+// $result = mysqli_fetch_all($res, MYSQLI_ASSOC);
+
+$dataTables = array();
+if (mysqli_num_rows($res) > 0) {
+	// output data of each row
+	while($row = mysqli_fetch_assoc($res)) {
+		$dataTables["data"][] = $row;
+	}
+} 
+
  
-echo json_encode($result);
+echo json_encode($dataTables);
