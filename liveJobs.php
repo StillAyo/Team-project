177de6bab1a3af66516c7fc0d5495a2af9c98ed4
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-14 17:02:19 +0000

    Update liveJobs.php

--- liveJobs.php
+++ liveJobs.php
@@ -12,5 +12,6 @@ $sql = "SELECT live.problem_no, type, personnel_id as CallerID, date FROM live I
 $res = mysqli_query($conn, $sql);
 // Fetch all
 $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
+die(mysqli_error($db)
  
 echo json_encode($result);
