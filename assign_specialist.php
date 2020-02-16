92f2938e2000d189ea62af277a7847fb61c7ad3e
StillAyo <52698719+StillAyo@users.noreply.github.com>
2020-02-16 12:30:33 +0000

    Update assign_specialist.php

--- assign_specialist.php
+++ assign_specialist.php
@@ -79,7 +79,7 @@ body {
 	//----------FUNCTION TO RETURN APPROPRIATE SPECIALIST--------------//
 	//Insert Into LIVE
 	$problem_no = $_SESSION["Problem_no"];
-	echo $problem_no;
+	echo "value is:  ".$problem_no."val";
 	$array = $_SESSION["problem_details"];
 	
 	$fields_values = array($problem_no, '123');
